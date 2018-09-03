92ddc9ec1adbcbb211e7633a3429fb64debeb016
Elf-mousE <ifmouz@gmail.com>
2018-09-04 06:56:13 +0800

    update official site

---
+++ data/select.js
@@ -0,0 +1,66 @@
+export const OPTIONS = [{
+    label: 'Bread, Cereal, Rice, and Pasta',
+    value: 'grains'
+  },
+  {
+    label: 'Vegetables',
+    value: 'vegetables',
+    disabled: true
+  },
+  {
+    label: 'Fruit',
+    value: 'fruit'
+  },
+  {
+    label: 'Milk, Yogurt, and Cheese',
+    value: 'dairy'
+  },
+  {
+    label: 'Meat, Poultry, Fish, Dry Beans, Eggs, and Nuts',
+    value: 'meat'
+  },
+  {
+    label: 'Fats, Oils, and Sweets',
+    value: 'fats'
+  }
+];
+
+export const PROVINCES = [{
+    value: 1,
+    label: 'Beijing'
+  },
+  {
+    value: 2,
+    label: 'Shanghai'
+  },
+  {
+    value: 3,
+    label: 'Guangdong'
+  }
+];
+
+export const CITIES = [
+  [],
+  [{
+      value: 11,
+      label: 'Haidian'
+    },
+    {
+      value: 12,
+      label: 'Chao Yang'
+    }
+  ],
+  [{
+      value: 21,
+      label: 'Huangpu'
+    },
+    {
+      value: 22,
+      label: 'Xuhui'
+    }
+  ],
+  [{
+    value: 31,
+    label: 'Guangzhou'
+  }]
+];
